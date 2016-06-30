5de82e2f2663f440284f361edf9ef068680b5741
keeying <wenkun0526@live.cn>
2016-07-01 03:42:33 +0800

    git push

--- src/HttpAjax.js
+++ src/HttpAjax.js
@@ -1,30 +1,30 @@
-var HttpAjax = (function () {
-  // 公用空函数
-  var empty = function () {}
-  var createXHR = function () {
-    var xhr
-    if (typeof XMLHttpRequest !== 'undefined') {
-      xhr = new XMLHttpRequest()
-      createXHR = function () {
-        return new XMLHttpRequest()
-      }
-    } else {
-      try {
-        xhr = new ActiveXObject('Msxml2.XMLHTTP')
-        createXHR = function () {
-          return new ActiveXObject('Msxml2.XMLHTTP')
+var HttpAjax = (function() {
+    // 公用空函数
+    var empty = function() {};
+    var createXHR = function() {
+        var xhr;
+        if (typeof XMLHttpRequest !== 'undefined') {
+            xhr = new XMLHttpRequest();
+            createXHR = function() {
+                return new XMLHttpRequest();
+            };
+        } else {
+            try {
+                xhr = new ActiveXObject('Msxml2.XMLHTTP');
+                createXHR = function() {
+                    return new ActiveXObject('Msxml2.XMLHTTP');
+                };
+            } catch (error) {
+                try {
+                    xhr = new ActiveXObject('Microsoft.XMLHTTP');
+                    createXHR = function() {
+                        return new ActiveXObject('Microsoft.XMLHTTP');
+                    };
+                } catch (error) {
+                    return null;
+                }
+            }
         }
-      } catch (error) {
-        try {
-          xhr = new ActiveXObject('Microsoft.XMLHTTP')
-          createXHR = function () {
-            return new ActiveXObject('Microsoft.XMLHTTP')
-          }
-        } catch (error) {
-          return null
-        }
-      }
+        return xhr;
     }
-    return xhr
-  }
-})
+});
